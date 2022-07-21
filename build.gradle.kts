853c44455731b15fd0ee94f926c1c446ed14f939
DreamOneX <me@dreamonex.eu.org>
2022-07-21 19:52:29 +0800

    style(build.gradle.kts): use space instead of tab

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-	implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
-	implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
 }
