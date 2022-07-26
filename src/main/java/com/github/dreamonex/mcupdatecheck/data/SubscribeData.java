e502b2c005f505b726317b19987c1101e07d52f8
DreamOneX <me@dreamonex.eu.org>
2022-07-26 19:00:14 +0800

    chore(license): copyright disclaimer

--- src/main/java/com/github/dreamonex/mcupdatecheck/data/SubscribeData.java
+++ src/main/java/com/github/dreamonex/mcupdatecheck/data/SubscribeData.java
@@ -1,12 +1,13 @@
 // Mirai Minecraft Update Checker
+// This file is part of Mirai Minecraft Update Checker
 // 	Copyright (C) 2022 DreamOneX
 //
-// 	This program is free software: you can redistribute it and/or modify
+// 	Mirai Minecraft Update Checker is free software: you can redistribute it and/or modify
 // 	it under the terms of the GNU Affero General Public License as
 // 	published by the Free Software Foundation, either version 3 of the
 // 	License, or any later version.
 //
-// 	This program is distributed in the hope that it will be useful,
+// 	Mirai Minecraft Update Checker is distributed in the hope that it will be useful,
 // 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 // 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // 	GNU Affero General Public License for more details.
